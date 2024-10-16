1b95f88e425d7dbdeac1355f1180b3bec9cfe9bb
Ubuntu <ubuntu@ip-10-0-0-62.ec2.internal>
2024-10-16 03:14:30 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+	region="us-east-1"
+}
+
+resource "aws_instance" "prem_tf_created"  {
+	instance_type="t2.micro"
+	ami="ami-0e86e20dae9224db8"
+	subnet_id="subnet-04edb3ea22236883e" 	
+}
