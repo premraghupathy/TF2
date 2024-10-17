9e8e2db4be85ae7c1b36e4d8b4ce794db2c692b9
Ubuntu <ubuntu@ip-10-0-0-62.ec2.internal>
2024-10-17 00:05:21 +0000

    added SG and web server

--- main.tf
+++ main.tf
@@ -6,6 +6,30 @@ resource "aws_instance" "prem_tf_created"  {
 	instance_type="t2.micro"
 	ami="ami-0e86e20dae9224db8"
 	subnet_id="subnet-04edb3ea22236883e" 	
+	vpc_security_group_ids = [aws_security_group.prem_tf_sg.id]
+
+	tags={
+		name="prem_tf_created_ec2"
+	}
+	
+	user_data=<<-EOF
+	#!/bin/bash
+	echo "TF launched web server" > index.html
+	nohup busybox httpd -f -p 8099 &
+	EOF
+
+	user_data_replace_on_change = true
+
 }
 
 
+resource "aws_security_group" "prem_tf_sg" {
+	name="tf_sg"
+	vpc_id="vpc-09792a9f84354af94"
+	ingress {
+	from_port = 8099
+	to_port = 8099
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
+}
